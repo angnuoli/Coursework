ba241b8fc88540ee73844b48cde16c7257aa2f8b
angnuo <angnuoli@gmail.com>
2018-02-08 22:21:05 -0500

    regression template

---
+++ Machine Learning/regression_template.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: gjxhlan
+"""
+
+# Regression template
+
+# 1. Data Preprocessing
+
+# Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:2].values # try to make X as matrix, y as vector
+y = dataset.iloc[:,2].values
+
+# =============================================================================
+# # Taking care of missing data
+# from sklearn.preprocessing import Imputer
+# imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+# imputer = imputer.fit(X[:, 1:3])
+# X[:, 1:3] = imputer.transform(X[:, 1:3])
+# 
+# # Encoding categorical data
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# labelencoder_X = LabelEncoder()
+# X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+# onehotencoder = OneHotEncoder(categorical_features = [0])
+# X = onehotencoder.fit_transform(X).toarray()
+# laberencoder_y = LabelEncoder()
+# y = laberencoder_y.fit_transform(y)
+# 
+# # Splitting the dataset into the Training set and Test set
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# 
+# # Feature Scaling
+# from sklearn.preprocessing import StandardScaler
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.fit_transform(X_test)
+# =============================================================================
+
+# 2. Fitting Regression Model to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+lin_reg.predict(X)
+
+# 3. Visualising the dataset results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, c='red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Salary vs Level')
+plt.xlabel('Level')
+plt.ylabel('Salary')
+plt.show()
+
