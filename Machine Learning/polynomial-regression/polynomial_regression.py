002120da0f1f56e8e7bd78b21991d1d1a691c656
angnuo <angnuoli@gmail.com>
2018-02-08 22:20:45 -0500

    polynomial regression

---
+++ Machine Learning/polynomial-regression/polynomial_regression.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: gjxhlan
+"""
+
+# Polynomial Regression
+
+# Data Preprocessing
+
+# Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:2].values # try to make X as matrix, y as vector
+y = dataset.iloc[:,2].values
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Visualising the dataset results
+plt.scatter(X, y, c='red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Salary vs Level')
+plt.xlabel('Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 3)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the dataset results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, c='red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Salary vs Level')
+plt.xlabel('Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result with Linear Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
